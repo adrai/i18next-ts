5466ad136ba01049282220a8800c3d5faf6e5d5a
Adriano Raiano <adriano@raiano.ch>
2020-09-17 16:42:22 +0200

    reformat

--- src/i18next.ts
+++ src/i18next.ts
@@ -1,11 +1,11 @@
 class I18next {
   constructor(){}
 
-  init (options: object) {
+  init(options: object) {
     console.log('inited', options)
   }
 
-  t (key: string, options?: object) : string {
+  t(key: string, options?: object) : string {
     return `whould have translated ${key}`
   }
 }
